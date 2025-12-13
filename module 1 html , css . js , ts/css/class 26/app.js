01d0e1e3e3ad232ea48fb59c27c9b498c7cbf76d
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-12-13 19:09:12 +0500

    js start

---
+++ module 1 html , css . js , ts/css/class 26/app.js
@@ -0,0 +1,19 @@
+
+
+alert('HELLO KESE HO')
+alert('Asslam o alikum')
+
+
+// 1) varaible
+// 2) loop
+// 3) condition
+// 4) function
+
+// WORD   
+// keyword  =>> wo word jo js me defined hon ya koi meaning nikalta ho js me
+// operator =
+
+// keyword variableName operator value
+var number = 10 + 20
+
+alert(number)
