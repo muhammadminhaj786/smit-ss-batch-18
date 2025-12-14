60eb6b18292e9a9b5ecf5e2e37be9aab8f92a0ca
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-12-14 19:09:48 +0500

    chapt1to4

---
+++ module 2 js , es6, TS/javaScript/class 27/app.js
@@ -0,0 +1,63 @@
+
+// alert('hi')
+// alert(2)
+
+// varaible
+// keyword varable_name operator value
+
+// string=> group of word
+
+"" , ''
+12 ,23
+var _username = 12
+var $name = 23
+var hello 
+//alert("number")
+hello = 'hello'
+//alert(hello)
+//window.alert('new')
+var name1 = "minhaj"
+//alert(name1)
+name1 = 'kashan'
+//alert(name1)
+
+// rules of varaible decralation
+// 1) variable name kabhi bhi number se start nhi hoga
+// 2) varable quotation me nhi lik sakte
+// 3) varaible name me space nhi de sakte
+// 4) varaible me special character  use nhi karsakte except ($, _)
+//5) varable name keyword ka name nhi de sakte
+
+console.log('hello')
+
+// var num1 = "5"
+// var num2 = 10
+// console.log(num1 + num2)
+
+var num4 = '5'
+var num5 = '10'
+console.log(num4 - num5)
+
+
+var num6 = 'minhaj'
+var num7 = 'kashan'
+console.log(num6 - num7)
+
+var num8 = 5 + 10
+var num9 = num8 + num8
+console.log(num9)
+
+// var class = '23'
+// console.log(alert)
+
+// var var = 23
+
+// camelCase
+var useremailforlogin = 'terst@gmal.com'
+// var userEmailForLogin
+
+// sanke_case
+// var user_email_for_login
+
+// PascalCase
+// var UserEmailForLogin 
