571360ea8148b1f39ee8e0883e3a0ef1176f3524
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-12-20 19:31:21 +0500

    chatper 7

---
+++ module 2 js , es6, TS/javaScript/class 28/app.js
@@ -0,0 +1,61 @@
+
+
+// alert('hi')
+// ALERT('HI')
+// pakistan  => word
+// alert => keyword
+// window.alert('Asslam o Alaikum')
+
+// window.alert()
+console.log('hi')
+
+var score = 20
+score = score + 2
+console.log(score)
+""
+var minhaj = 10
+var userName = minhaj
+var newUserName = userName
+var hello = newUserName
+console.log(hello)   //10
+minhaj = 20
+var userName = minhaj
+var newUserName = userName
+var hello = newUserName
+console.log("hello") 
+
+
+var score2 = 20
+
+// var @new
+
+console.log(2 - 2)
+
+var score1 = 20
+console.log(score1 + score2)
+
+console.log(20 % 6)
+
+var a = 2
+var b = 7
+
+console.log(a**2 + 2*(a)*(b) + b**2   )
+console.log(1 /0)
+
+console.log("2 * 1 = ", 2 * 1 )
+console.log("2 * 2 = ", 2 * 2 )
+
+// operators  + - * / % ** ++ 
+var num = 5
+// num = 1 + 1
+// console.log(num)
+console.log(++num)
+console.log(--num)
+console.log(num++)
+console.log(num)
+
+var a =2
+var b =3
+console.log((a + b) * a - b++ + --a + b )  //17 , 9, 8, 6, 7 , 12, 30
+
+12
