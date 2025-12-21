f5325ea524409a7fc7d23998758f095882bf25b1
muhammadminhaj786 <eb20103087.minhajwawhid@gmail.com>
2025-12-21 19:12:03 +0500

    condition

---
+++ module 2 js , es6, TS/javaScript/class 29/app.js
@@ -0,0 +1,43 @@
+console.log('hello')
+
+console.log((2+ 2) * 2 + 2)   //10
+console.log(1 + 3 * 4)
+
+
+console.log(2 * 4 * 4 + 2)  //34
+
+console.log(" " - " ")
+
+
+// var firstName = prompt('Enter your first name')
+// var lastName = prompt("Enter your last name")
+// console.log(firstName + " " + lastName )
+// console.log( "Thanks for your review " +  firstName)
+
+
+// alert(2 - (2 +"2 plus 2 equals ") )
+
+//4 
+//2 plus 2 equals  22
+//2 plus 2 equals  4
+
+// var a = Number(prompt('enter a value'))
+// var b = Number(prompt('enter b value'))
+// var c = String(5)
+
+// console.log(c - a + b )
+
+// console.log('five' + '5')
+
+
+//condition
+// if (condition) {
+
+// }
+
+// comparison operator
+if (2 ==3) {
+    alert('hn 2 ke barabar he')
+}else {
+    alert('nhi he barabar')
+}
