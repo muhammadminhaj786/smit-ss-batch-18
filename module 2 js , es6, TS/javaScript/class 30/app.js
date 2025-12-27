9417a817db418ab2f8c8ceb1fd19d05aa7823a9a
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2025-12-27 19:27:37 +0500

    condition

---
+++ module 2 js , es6, TS/javaScript/class 30/app.js
@@ -0,0 +1,80 @@
+console.log('4' * '4')
+
+"=== > < >="
+
+// if (2+2 != "4") {
+//     console.log('true')
+// }else {
+//     console.log('false')
+// }
+
+// if (2+2 === "4") {
+//     console.log('true')
+// }else {
+//     console.log('false')
+// }
+
+// if (2+2 !== '4') {
+//     console.log('true')
+// }else {
+//     console.log('false')
+// }
+
+// var age = prompt('Enter your age')
+// console.log(age)
+// if (age >= 18){
+//     console.log('allowed')
+// }else {
+//     console.log('not allowed')
+//     window.close()
+// }
+
+// var correctAnswer = 'karachi'
+// var secondCorrectAnswer = 'lahore'
+// var cityName = prompt('which city called city of lights?').toLocaleLowerCase()
+// console.log(cityName.toLowerCase())
+// var transformName = cityName.toLocaleLowerCase()
+// if (transformName == correctAnswer) {
+//     alert('correct')
+// }else if (cityName == secondCorrectAnswer) {
+//     alert('correct')
+// }
+// else {
+//     alert('wrong answer')
+// }
+// if (cityName.toLocaleLowerCase() == correctAnswer) {
+//     alert('correct')
+// }else {
+//     alert('wrong answer')
+// }
+
+// var dieseas = 'covid'
+// var symptoms = 'cough'
+// var userDiseses = prompt('Enter your diseses').toLocaleLowerCase()
+// var userSymptoms = prompt('Enter your symptoms').toLocaleLowerCase()
+
+// if (dieseas == userDiseses){
+//     alert('eligible')
+// }else if (symptoms == userSymptoms) {
+//     alert('eligible')
+// }
+// else {
+//     alert('not eligible')
+// }
+
+// '& | !'
+
+// var age = prompt('Enter your age')
+// if (age >= 18 && age <= 40 ){
+//     alert('allowed')
+// }else {
+//     alert('not allowed')
+// }
+
+
+'0 false nan undifined null "" ! '
+if(""){
+    console.log('true')
+}else {
+    console.log('false')
+}
