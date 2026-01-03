ef54b4446bc20119328b745f6be14231b0c8a2fd
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2026-01-03 19:22:02 +0500

    loop

---
+++ module 2 js , es6, TS/javaScript/class 32/app.js
@@ -0,0 +1,87 @@
+// var a = 2
+// var b= 1
+
+// // --a - --b + ++b + b--;
+// // var result = --a - --b + ++b + b--
+// //             // 1     - 0  + 1  + 1
+// //             console.log(result)
+
+// var result2 =  a++ - --b + b-- + ++a + b-- + ++a - ++b;
+// //    2  -  0   +  0  + 4   - 1   + 5  + 1
+// //    2 + 4 - 1 + 5  + 1
+// //    11
+// console.log(result2)
+
+// var a = 10 
+// var b= 8
+// const result3 = --a + ++b - b-- + --b + ++a - b-- + a + b + b++ + b
+
+// console.log(result3)
+
+// var color = "blue"
+// var num2 = 2
+
+// if (1) {
+//     if (2 == num2){
+//         console.log('true')
+//     }
+// }else {
+//     console.log('false')
+// }
+
+// var city = ['karachi', 'lahore', "islamabad", "pindi"]
+//                 // 0   ,   1,      2,             3
+// // console.log(city[3])
+// // city[3] = "multan"
+// // console.log(city)
+
+// // //array methods
+// // // push 
+// // city.push("hyderabad", "kashmir", "peshawar")
+// // console.log(city)
+
+// // //POP
+// // city.pop()
+// // console.log(city)
+
+// // //unshift 
+// // city.unshift('hyderabad')
+// // console.log(city)
+
+// // //shift 
+// // city.shift()
+// // console.log(city)
+
+// console.log(city)
+// city.splice(2, 0,"sukhar", "gilgit")
+// console.log(city)
+// //  ['karachi', 'lahore', 'sukhar', 'gilgit', 'islamabad', 'pindi']
+
+//                     // index, length
+// var newArr = city.slice(2, 5)
+// console.log(newArr)
+
+
+// loop
+
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+// console.log('hello')
+
+
+for (var num = 1;num < 11; num ++){
+   
+   console.log('hello', num) 
+}
+
