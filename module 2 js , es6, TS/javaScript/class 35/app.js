2fdff50be9ce99f50e41fb529b756eb4210e9847
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2026-01-11 19:24:43 +0500

    objects

---
+++ module 2 js , es6, TS/javaScript/class 35/app.js
@@ -0,0 +1,102 @@
+
+
+// for (var i = 1; i<= 5; i++){
+//     var stars = ""
+//     // console.log(stars)
+//     for (var j= 0; j < i; j++){
+//         stars = stars + "*"
+//     }
+//     console.log(stars)
+// }
+
+
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// [
+//     {
+//     name: 'ahmed', 
+//     rollNo:  123,
+//     subjects: [
+//         'english', "urdu", 'math'
+//     ],
+//     addrees: {
+//         country: 'pakistan',
+//         city: 'karachi',
+//         stree: 'nazimabad no 2 karachi'
+//     },
+//     test
+  
+// },
+//  {
+//     name: 'ahmed', 
+//     rollNo:  123,
+//     subjects: [
+//         'english', "urdu", 'math'
+//     ],
+//     addrees: {
+//         country: 'pakistan',
+//         city: 'karachi',
+//         stree: 'nazimabad no 2 karachi'
+//     },
+//     test
+  
+// }
+// ]
+
+var test = 123
+// var name ="minhaj"
+var test2
+var std1 = {
+    test2,
+    rollNo:  [123],
+    subjects: [
+        'english', "urdu", 'math'
+    ],
+    addrees: {
+        country: 'pakistan',
+        city: 'karachi',
+        stree: 'nazimabad no 2 karachi'
+    },
+    test,
+  
+}
+// var std2 = {
+//     test2,
+//     rollNo:  [345, ],
+//     subjects: [
+//         'english', "urdu", 'math'
+//     ],
+//     addrees: {
+//         // country: 'pakistan',
+//         city: 'karachi',
+//         stree: 'nazimabad no 2 karachi'
+//     },
+//     test,
+  
+// }
+// std2.rollNo.push(std1.rollNo)
+console.log("hello")
+// console.log(std2.rollNo,std1.rollNo )
+
+console.log(std1.rollNo)
+console.log(std1.addrees.city)
+
+// var std2;
+
+// console.log(std2.test)
+console.log(std1)
+// console.log(std2.addrees.country.abc  )
+
+// console.log(num)
+
+// // var num1 
+// var num2 = num1
+// console.log(num2)
+
+// var newArr = [[0,1],[2,3]]
+// console.log(newArr[1][1])
