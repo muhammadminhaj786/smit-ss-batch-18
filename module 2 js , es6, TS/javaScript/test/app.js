6be85320adf7d87cea84cc7053aaf0d4f53b1ced
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2026-01-11 19:28:52 +0500

    s

---
+++ module 2 js , es6, TS/javaScript/test/app.js
@@ -0,0 +1,112 @@
+
+
+// var num = [1,2,3,4,5,6,7,8,9,10]
+
+// // console.log(num[0])  
+
+// var flag = false
+
+// for (var i =0; i <num.length; i++){
+//     console.log(num[i])
+//     if (num[i] == 5){
+//         flag = true
+//         //  alert('found')
+//         break
+//     }else {
+//         flag = false
+//         //   alert('not found')
+//     }
+// }
+
+// if (flag){
+//     alert('found')
+// }else {
+//     alert('not found')
+// }
+
+
+//nested loop
+
+// for (var i = 1 ; i <= 5 ; i++){
+//     for (var j = 1 ; j <= 3; j ++){
+//          console.log(i ,"=",j)
+//     }
+//     // console.log(i)
+// }
+
+
+// for (var i = 1 ; i <= 5 ; i++){
+//     var stars = ""
+//     for (var j = 1 ; j <= i; j ++){
+//         //  console.log(i ,"=",j)
+//         stars += "1"                
+//         //  console.log(stars)
+//     }                                                          //*
+//                                                                 //*
+//     // stars = "*"
+//     console.log(stars)
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// ***
+// ***
+// ***
+// ***
+// ***
+// ***
+// ***
+// ***
+// ***
+// ***
+// ***
+
+
+
+// *****
+// ****
+// ***
+// **
+// *
+
+// for(var i = 20; 1 <= i; i -=2){
+//    var star = "";
+//    for(var j = 1; j <= i; j++){
+//     star += "*";
+//    }
+//    console.log(star + i);
+// }
+
+
+//object 
+
+var students = [std1]
+// var teacher = [tea1]
+
+// {
+//     key : 'value',
+// }
+
+var std1 = {
+    firstName: 'wasim',
+    lastName: 'shah',
+    rollNo: 123,
+    phoneNo: "213434",
+    isActive: true,
+    "0": 1
+}
+
+var std1 = {
+    firstName: 'wasim',
+    lastName: 'shah',
+    rollNo: 123,
+    phoneNo: "213434",
+    isActive: true,
+    "0": 1
+}
+
+console.log(std1.rollNo)
