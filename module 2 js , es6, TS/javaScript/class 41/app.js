85a6bf38b0181fe81862cc30b4237ff469af2f5f
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2026-02-01 19:32:53 +0500

    function passing back

---
+++ module 2 js , es6, TS/javaScript/class 41/app.js
@@ -0,0 +1,65 @@
+
+sum(2,3)
+
+// var num1= undefined
+
+
+// function test(){
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// console.log("hello")
+// // test()
+
+// }
+
+// test()
+
+
+// parameter , argument
+// function foo(parameter){
+
+// }
+
+// foo(argument)
+
+
+// var num1 = +prompt('Enter first number', 5)
+// var num2 = +prompt('Enter second number')
+
+// function foo(a=2,b=5){
+
+// // console.log(b)
+// var sum = a + b
+// console.log(sum)
+
+// }
+
+// foo(0,num2)
+
+// function greetUser(a){
+//     alert('hello ' + a)
+// }
+
+// greetUser('minhaj')
+function sum(a,b){
+    var add = a + b
+    console.log(add) 
+    // return sum(1, 5)
+    return(1,2,3,4,5)
+
+
+}
+
+
+prompt("What you want to change \n1. KPH to MPH \n 2. Area\n 3.temprature")
+// var output = sum(3,4)
+// console.log(output)
+// sum(3,4)
+
+// hoisting
+// console.log(num1)
+// const num1 = 1
+// console.log(num1)
