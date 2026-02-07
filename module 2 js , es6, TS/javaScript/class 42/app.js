ce95a7d887aa86d45eef3a9d73afaba421a8dfa6
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2026-02-07 19:34:22 +0500

    var, let ,const

---
+++ module 2 js , es6, TS/javaScript/class 42/app.js
@@ -0,0 +1,103 @@
+// // console.log(user_name)
+
+
+// var user = prompt('Enter conversation method , Please choose option \n 1 = kilmoter \n 2 = temperature')
+
+// // if(user.toLowerCase() == "kilometer")
+
+// if(user == '1'){
+//     killometer()
+// }else if (user == '2'){
+//     temp()
+// }else {
+//     alert('Please choose correct')
+// }
+
+let cal ; //100011
+function killometer (){
+    var killo_number = +prompt('Enter killometers')
+     let cal = killo_number * 1000  //10010
+    alert("your total meter are" + cal)
+}
+killometer()
+console.log(cal)
+
+
+
+function temp(){
+    var celcius = + prompt('Enter Degree in celcius')
+    const cal = (celcius * 1.8) + 32
+    alert('your degree in Fahrenheit is' + " " + cal + "°F")
+}
+
+
+// var user_name = 'minhaj'
+// console.log(user_name)
+
+// var num2 ;
+// console.log(num2)
+
+//  var num = 12
+//  var num2;
+// function say_name(){
+//     // var num2 = 10
+//     num2 = 10
+//    console.log(num)
+//     console.log('hello')
+//     if(true){
+//         console.log(num)
+//     }
+
+// }
+// say_name()
+// console.log(num2)
+
+// if(true){
+
+// }
+
+// var num3 = 4
+// var num3 = 5
+
+// let num3 = 4
+// let num3 = 7
+
+let num3 = 2
+if(true){
+    console.log(num3)
+    let num4 = 20
+}
+// console.log(num4)
+
+function foo(){
+    let num5 = 30
+    // const num6;
+    if(true){
+        console.log(num5)
+        // let num6 = 10
+        // console.log(num6)
+    }
+    // console.log(num6)
+
+}
+foo()
+// console.log(num5)
+
+// let num8 = 2
+// let num8 = 8
+
+//  const num1 = 9
+//  const num1 = 10
+
+console.log(num10)
+let num10 = 10
+// tdz= >temperor dead time zone
+
+//var function scope redecred
+//let block scope not  re decread in same scope
+// const block scope not redecrealed and reintialized 
+
+function foo(){
+    var num2 = 10
+}
+console.log(num2)
