089f14d6e5725ce1654ad0442bda8ce6bd83357d
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2026-02-08 19:26:54 +0500

    while, do-while, switch

---
+++ module 2 js , es6, TS/javaScript/class 43/app.js
@@ -0,0 +1,125 @@
+// var now = new Date()
+// // var day = now.getDay()
+// var day = 7
+// // console.log(now.getDay())
+
+// // if(day == 0){
+// //     console.log('sunday')
+// // }else if(day == 1){
+// //     console.log('monday')
+// // }
+// // else if(day == 2){
+// //     console.log('tuesday')
+// // }else if(day == 3){
+// //     console.log('wednesday')
+// // }else if(day == 4){
+// //     console.log('thursday')
+// // }else if(day == 5){
+// //     console.log('friday')
+// // }else if(day == 6){
+// //     console.log('saturday')
+// // }
+
+// // switch(expersion) {
+// //     case 1:
+// //         code
+// //         break
+// //     case 2:
+// //         code
+// //         break
+// // }
+
+// // switch (day){
+// //     case 0:
+// //         console.log('sunday')
+// //         break
+// //     case 1:
+// //         console.log('monday')
+// //         break
+// //     case 2:
+// //         console.log('tuesday')
+// //         break
+// //     case 3:
+// //         console.log('wednesday')
+// //         break
+// //     case 4:
+// //         console.log('thursday')
+// //         break
+// //     case 5:
+// //         console.log('friday')
+// //         break
+// //     case 6:
+// //         console.log('saturday')
+// //         break
+// //     default:
+// //         console.log('not a day')
+// //         break
+// // }
+
+// var food = prompt('Enter item')
+// switch (food){
+//     case 'biryani':
+//         var varaint = prompt("konsi biryani chaiye \n 1.Chicken \n 2.Beef \n 3.nali")
+//         var item_price = {
+//             chicken: '250',
+//             beef: '500',
+//             naali: '450',
+
+//         }
+//         console.log('apki ' + varaint + " biyani avalaible he or ye " + item_price[varaint] +" rupee ki he" )
+//         // switch (varaint){
+//         //     case "chicken"
+//         // }
+//         break
+//     case 'pizza':
+//         console.log('avaliable')
+//         break
+//     default:
+//         console.log('not avalaible')
+// }
+
+// // if(food == 'biryani'){
+// //     console.log('avaliable')
+// // }else if(food == 'pizza'){
+// //     console.log('avaliable')
+// // }
+
+// var item_price = {
+//   chicken: "250",
+//   beef: "500",
+//   naali: "450",
+// };
+// // // dot notation
+// let chicken22 = "chicken";
+// console.log(item_price[chicken22]);
+
+// //array notation
+// console.log(item_price[chicken])
+
+
+// for(var i=1; i<4 ; i++){
+//     console.log(i)
+// }
+
+// while (condition){
+//     code
+// }
+
+// for(intilization; condition;counter){
+
+// }
+for(var i =0; i<3; ){
+    console.log(i)
+    i++
+}
+
+var i =0 
+
+/*
+do{
+    alert(i)
+    // i = i +1
+    i++
+}
+while (i < 3)
+*/
