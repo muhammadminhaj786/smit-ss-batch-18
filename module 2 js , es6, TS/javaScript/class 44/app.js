2d66f92ed66304ea90802db4675ea56d3b6e448d
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2026-02-14 19:31:01 +0500

    dom

---
+++ module 2 js , es6, TS/javaScript/class 44/app.js
@@ -0,0 +1,74 @@
+
+// number=>Integer, float 1
+// string=>"", '1'
+// Boolean true false
+// 'true'
+// var true = '123'
+
+
+// 'i've car'
+// "this said "something"
+
+// var => function, re initlize, redecread
+// let => block , re initlize
+// const  => block
+// var tax = 2.5
+// var tax = 3
+// var num = 123
+// let num2 =  123
+// let num2= 567
+// const num3 = 123
+// // const num3 = 456
+// num3 = 123
+// var const = 123
+
+
+// tax = undefined
+
+// var hello; ==>decrealed
+// hello = 123   =>intilize
+// console.log(hello)
+// var hello = 23
+
+// var num3 = 1
+// var num3 = [1,2,3,'5']
+// var num3 = {
+//     key: value
+// }
+
+// var i =0
+// do{
+//     console.log(i)
+//     i++
+// }while(i>10)
+
+// var mobile = false
+// var flag = false
+// var dish = ['biryani', 'qorma', 'krahi', 'handi', 'pizza', "fries"]
+// for (var i=0; i<dish.length; i++){
+//     console.log(i)
+    // if(mobile)
+    // if(dish[i]=='tikka'){
+    //     flag = true
+    //     break
+    // }
+    // else{
+    //       flag = false
+    // }
+// }
+
+// if(flag){
+//     alert('found')
+// }else {
+//     alert('not found')
+// }
+// while()
+
+// dom document object model
+
+console.log(document)
+// createElement('div')
+// getElement
+// console.log(document.getElementById('box').c)
+// console.log(document.querySelector("box"))
+// document.appendChild
