2a6ffdbfb269ee5844b83fdd17228a4663fd873c
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2026-02-28 15:22:42 +0500

    login form complete

---
+++ module 2 js , es6, TS/javaScript/class 47/app.js
@@ -0,0 +1,27 @@
+// .value => value get from input
+// setAttribute set karata he attribute ko
+
+
+// var emailInp = document.getElementById('email')
+// var passInp = document.getElementById('pass')
+// var emailPara = document.getElementById('emailPara')
+// var passPara = document.getElementById('passPara')
+
+// email = 'admin@gmail.com'
+// password = 'admin123'
+// function getValue(){
+//     console.log(emailInp)
+//     console.log(emailInp.value)
+//     console.log(passInp.value)
+//     if(emailInp.value == email && passInp.value == password){
+//         alert('login successfully')
+//         window.location.href = 'home.html'
+//     }else{
+//         // emailInp.style.border = '2px solid blue'
+//         emailPara.innerText = 'Invalid'
+//         passPara.innerText = 'Invalid password'
+//         emailInp.setAttribute('class', 'error')
+//         passPara.setAttribute('class', 'text-danger text-center ')
+//         alert('invalid credentials')
+//     }
+// }
