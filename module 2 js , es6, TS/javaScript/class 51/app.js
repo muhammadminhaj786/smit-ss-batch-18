3ffc6bad230d4deb7da7685003c62da9c703a64e
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2026-03-28 19:23:32 +0500

    objec constructor

---
+++ module 2 js , es6, TS/javaScript/class 51/app.js
@@ -0,0 +1,52 @@
+
+console.log('hello')
+// alert('hi')
+// var firstName = "ali"
+// console.log('again',  'hi')
+// prompt('hi')
+
+var light = "green"
+if (light == 'green') {
+    console.log('run')
+}else {
+    console.log('stop')
+}
+
+var colors = ['red', 'green']
+
+// for (var i = 0;i < 10 ; i++){
+
+// }
+
+let a =null
+function foo(){
+     a = 12
+}
+console.log(a)
+
+function pressClick(a){
+    console.log('hello',a)
+}
+
+var obj = {
+    firstName: 'minhaj',
+    lastName: 'wahid',
+    greet: function (a){
+        console.log('hello', a)
+    }
+}
+console.log(obj.firstName)
+console.log(obj['lastName'])
+// obj.greet('ali')
+// obj['greet()']
+var obj2 = {
+       firstName: 'ali',
+    lastName: 'raza',
+    greet: function (a){
+        console.log('hello', a)
+    }
+}
+
+// function Dummy(){
+
+// }
